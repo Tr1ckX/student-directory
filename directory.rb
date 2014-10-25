35cbf6d945b9f700fabba3ab9b6d27a29c86e22a
Tr1ckX <attila.mirk@gmail.com>
2014-10-25 11:43:02 +0100

    comments added

--- directory.rb
+++ directory.rb
@@ -1,3 +1,4 @@
+# first we print the list of students
 puts "\n"
 puts " The students of my cohort at Makers Academy"
 puts "---------------------"
@@ -7,6 +8,8 @@ puts "Chris H"
 puts "Izzy M"
 puts "Nick D"
 puts "Attila M"
+# finally we print the total
 print "Overall, we have "
+# print() does not add new line characters
 print 6
 print " great student"
