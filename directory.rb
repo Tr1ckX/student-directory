34699ae7df546950d1d170eb51f8b6b60f52404a
Tr1ckX <attila.mirk@gmail.com>
2014-10-25 15:32:59 +0100

    User input to get students' name

--- directory.rb
+++ directory.rb
@@ -1,13 +1,27 @@
-
-# let's put all students into an array
-students = [
-{name: "Ben H", cohort: :october},
-{name: "Rich L", cohort: :october},
-{name: "Chris H", cohort: :october},
-{name: "Izzy M", cohort: :october},
-{name: "Nick D", cohort: :october},
-{name: "Attila M", cohort: :october}
-]
+def input_students
+	puts "Please enter the name of the students"
+	puts "To finish, just hit enter twice."
+	
+	#create an empty array
+	students = []
+	
+	#get the first name
+	name = gets.chomp
+	
+	#while the name is not empty, repeat this code
+	while !name.empty? do
+		
+		#add the student hash to the array
+		students << {name: name, cohort: :november}
+		puts "Now we have #{students.length} students."
+		
+		#get another name from the user
+		name = gets.chomp
+	end
+	
+	#return the array of students
+	students
+end
 
 def print_header
 	puts "\n"
@@ -28,7 +42,7 @@ def print_footer(names)
 	puts "Overall, we have #{names.length} great student"
 end
 
-#nothing happens until we call the methods
+students = input_students
 print_header
 print(students)
 print_footer(students)
