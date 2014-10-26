e583c0fa779e3f660d925bef2867ebce62819772
Tr1ckX <attila.mirk@gmail.com>
2014-10-26 07:46:13 +0000

    interactive menu

--- directory.rb
+++ directory.rb
@@ -1,6 +1,48 @@
+
+def interactive_menu
+	
+	students =[]
+	
+	loop do
+		
+		# 1. print the menu and ask the user what to do
+		puts "1. Input the students"
+		puts "2. Show the students"
+		puts "9. Exit"
+		
+		# 2. read the input and save it
+		selection = gets.chomp
+		
+		# 3. do what user asked
+			case selection
+				
+				when "1"
+					students = input_students
+				
+				when "2"
+					print_header
+					print_out(students)
+					print_footer(students)
+				
+				when "9"
+					exit #program termination
+					
+				else
+					puts "I dont know what you meant, try again."
+			end
+	end
+end
+
+
+
+
+
+
+
+
 def input_students
-	puts "Please enter the name of the students"
-	puts "To finish, just hit enter twice."
+	print "Please enter the name of the students\n"
+	print "To finish, just hit enter twice.\n"
 	
 	#create an empty array
 	students = []
@@ -13,7 +55,7 @@ def input_students
 		
 		#add the student hash to the array
 		students << {name: name, cohort: :november}
-		puts "Now we have #{students.length} students."
+		print "Now we have #{students.length} students.\n"
 		
 		#get another name from the user
 		name = gets.chomp
@@ -24,25 +66,23 @@ def input_students
 end
 
 def print_header
-	puts "\n"
-	puts " The students of my cohort at Makers Academy"
-	puts "---------------------"
+	print " The students of my cohort at Makers Academy\n"
+	print "---------------------\n"
 end
 
-def print(students)
+def print_out(students)
 
-	students.each do |student|
-		puts "#{student[:name]} (#{student[:cohort]} cohort)"
+	students.each.with_index(1) do |student, index|
+		
+		print "#{index} #{student[:name]} (#{student[:cohort]} cohort)\n" if student[:name].start_with?("a")
+	
 	end
 
 end
 
 
 def print_footer(names)
-	puts "Overall, we have #{names.length} great student"
+	print "Overall, we have #{names.length} great student\n"
 end
 
-students = input_students
-print_header
-print(students)
-print_footer(students)
+interactive_menu
